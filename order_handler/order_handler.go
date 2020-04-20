012c7e6f4f8d4d393bc29b29efe31a5446417975
stiangh <32125631+stiangh@users.noreply.github.com>
2020-04-20 15:29:39 +0200

    order_handler done?

--- order_handler/order_handler.go
+++ order_handler/order_handler.go
@@ -5,140 +5,150 @@ import "../requests"
 import "../fsm"
 import "../elevator"
 import "../timer"
-import "/..elevstate"
-import "../peers"
+import "../elevstate"
 import "math"
 
-type ElevQueue struct {
-	QueueSystem [4][4]int
-	CabCall     [4]int
-	HallCall    [4][2]int
-	ID          string
-}
-
-type Order struct {
-    Floor int
-    Button elevio.ButtonType
-    AssignedTo string //hvilken heis som har orderen
-}
-
-
-
-func DelegateOrder(elevID string, btn chan<- elevio.ButtonEvent,  assignedOrder <-chan Order){ //skal sende ordren til den valgte heisen enten lokalt (til master) eller over nettet (til en slave)
-    netSend := make(chan Order)
-    netRecv := make(chan Order)
-
-    //TODO: legge til hvordan kanalene sendes over nettet
-
-
-
-    for{
-
-        select{
-
-        case orderReceived := <- assignedOrder:
-
-            if btn! =2 { //ikke cabcall
-                //TODO: mulig det må legges til litt som gjør at dette alltid fungerer
-                netSend <- orderReceived
-
-            }
-
-            if orderReceived.AssignedTo == elevID || orderReceived.Button == 2 {// er cabcall
-                btn <- elevio.ButtonEvent{Floor: orderReceived.Floor, Button: int(orderReceived.Button)}//litt skeptisk til denne
-
-                elevio.SetButtonLamp(orderReceived.Button, orderReceivedFloor, true)
-            }
-
-        case orderReceived := <- netRecv:
-            elevio.SetButtonLamp(orderReceived.Button, orderReceivedFloor, true)
-
-            if orderReceived.AssignedTo == elevID {
-				btn <- elevio.ButtonEvent{Floor: orderReceived.Floor, Button: int(orderReceived.Button)} //litt skeptisk til denne
-
-        }
-
-    }*/
-
-
-
-}
-
-func AssignNewOrder(elevID string, buttonPressed <- chan elevio.ButtonEvent, allStates <-chan map[string]elevator.Elevator, peerUpdate <-chan peers.PeerUpdate, assignedOrder chan Order)
-// skal registrere en ny ordre og finne den beste heisen for denne ordren
-
-var activeElevators []string
-var states map[string]elevator.Elevator
-
-
-    for{
-        select {
-
-        case updatedStates := <- allStates:
-            states = updatedStates
-
-
-        case newElevator := <- elevatorUpdate:
-            activeElevators = newElevator.Peers
-
-
-        case btn := <- buttonPressed:
-
-            currentStates := make(map[string]elevator.Elevator)
-
-            for _, id:= range activeElevators {
-                if state, ok:=states[id]; ok {
-                    currentStates[id] = state
-                }
-            }
-
-            bestElev := findBestElev(btn, elevID, currentStates )
-
-            newOrder := Order{btn.Floor, btn.Button, bestElev}
-
-            assignedOrder <- newOrder
-
-        }
-
+// se på SystemState.json, cleare gamle unassignedRequests, extract nye ordre
+func handleSystemStateFromMaster() (newOrders []elevio.ButtonEvent) {
+  systemState, err := elevstate.RetrieveSystemState()
+  
+  if err != nil {
+    log.Println(err)
+    return
+  }
+  
+  localState, err := elevstate.RetrieveState()
+  
+  if err != nil {
+    log.Println(err)
+    return
+  }
+  
+  // Finner master sin versjon av state
+  var remoteState elevstate.State
+  for i, state := range systemState.states {
+    if state.ID == localState.ID {
+      remoteState = state
+      break
     }
+  }
+  
+  // Clearer de unassignedRequestene (fra localState) som master har sett
+  for _, r1 := range remoteState.unassignedRequests {
+    for i, r2 := range localState.unassignedRequests {
+      if r1.Floor == r2.Floor && r1.Button == r2.Button {
+        // Slette ett element på index i
+        copy(localState.unassignedRequests[i:], localState.unassignedRequests[i+1:])
+        localState.unassignedRequests[len(localState.unassignedRequests) - 1] = {}
+        localState.unassignedRequests = localState.unassignedRequests[:len(localState.unassignedRequests)-1]
+        
+        break
+      }
+    }
+  }
+  
+  // Extract nye ordre
+  newOrders = remoteState.newOrders // så enkelt ? 
+  //remoteState.newOrders = []
+  //localState.NewOrders = append(localState.NewOrders, newOrders...)
+  localState.NewOrders = newOrders
+  
+  // Lagre State
+  elevstate.StateStore(localState)
+  // Tenker da ikke å sende state tilbake til master med en gang, etter som det vil gjøres når chaffeur tar imot de nye ordrene
 }
 
-
-
-
-
-
-func findBestElev(hallBtn elevio.ButtonEvent , elevID string, states map[string]elevator.Elevator  ){ //finner den beste heisen ut i fra kostfunksjonen
-
-	bestTime := math.MaxInt64
-	bestElev := elevID
-
-	for id, state := range states {//går gjennom alle states til hver elevID
-        state.Requests[btn.Floor][btn.Button] = 2
-		if timeToIdle(id) < bestTime{
-			lowestCost = timeToIdle(id)
-			bestElev = id
-		}
-
-	}
-
-	return bestElev //string
-
-
+// ta inn staten til slaven som string (json), cleare newOrder i SystemState.json, se etter unassigned hos alle slaver
+func handleStateFromSlave(slaveState elevstate.State) (unassignedRequests []elevio.ButtonEvent) {
+  systemState, err := elevstate.RetrieveSystemState()
+  
+  if err != nil {
+    log.Println(err)
+    return
+  }
+  
+  // Finner index i system
+  var systemIndex int
+  for i, state := range systemState.states {
+    if state.ID == slaveState.ID {
+      systemIndex = i
+      break
+    }
+  }
+  
+  // Clearer de newOrder-ene (fra system) som slave har sett
+  for _, r1 := range slaveState.NewOrders {
+    for i, r2 := range  systemState.states[i].NewOrders {
+      if r1.Floor == r2.Floor && r1.Button == r2.Button {
+        // Slette ett element på index i
+        copy(systemState.states[systemIndex].NewOrders[i:], systemState.states[systemIndex].NewOrders[i+1:])
+        systemState.states[systemIndex].NewOrders[len(systemState.states[systemIndex].NewOrders) - 1] = {}
+        systemState.states[systemIndex].NewOrders = systemState.states[systemIndex].NewOrders[:len(systemState.states[systemIndex].NewOrders)-1]
+        
+        break
+      }
+    }
+  }
+  
+  unassignedRequests = slaveState.unassignedRequests
+  
+  systemState.states[systemIndex].unassignedRequests = unassignedRequests
+  systemState.states[systemIndex].Floor = slaveState.Floor
+  systemState.states[systemIndex].Dirn = slaveState.Dirn
+  systemState.states[systemIndex].Requests = slaveState.Requests
+  
+  err = elevstate.SystemStore(systemState)
+  
+  if err != nil {
+    log.Println(err)
+  }
 }
 
-func timeToIdle(e elevator.Elevator) int {//finner tiden det tar før heisen er "fri" (idle) til gjennomføre en ny request
+func AssignNewOrder(newRequest elevio.ButtonEvent) {
+  
+  systemState, err := elevstate.RetrieveSystemState()
+  
+  bestIndex := findBestElevIndex(newRequest, systemState.states)
+  
+  systemState.states[bestIndex].newOrders = append(systemState.states[bestIndex].newOrders, newRequest)
+  
+  systemState.HallLights[newRequest.Floor][newRequest.Button] = true
+  
+  err = elevstate.SystemStore(systemState)
+}
+
+func findBestElevIndex(hallBtn elevio.ButtonEvent , states []elevstate.State  ) int { 
+  bestTime := math.MaxInt64
+  bestIndex = 0
+  for i, state := range states {
+    e := elevator.Uninitialized()
+    e.Floor = state.Floor
+    e.Dirn = state.Dirn
+    e.Requests = state.Requests
+    e.Requests[btn.Floor][btn.Button] = true
+    
+    tti := timeToIdle(e)
+    if tti < bestTime {
+      bestTime = tti
+      bestIndex = i
+    }
+  }
+
+	return i //int
+}
+
+func timeToIdle(e elevator.Elevator) int {
     const TRAVEL_TIME := 2000
     const DOOR_OPEN_TIME := timer.Start(e.config.DoorOpenDuration)
     D_Stop := elevio.MD_Stop
 
     duration := 0;
-
+    
     switch e.State {
     case elevator.EB_Idle:
         e.Dirn = requests.ChooseDirection(e);
         if e.dirn == D_Stop {
-            return duration; //returnerer fordi heisen idle = "fri"
+            return duration;
         }
         break;
 
@@ -156,24 +166,12 @@ func timeToIdle(e elevator.Elevator) int {//finner tiden det tar før heisen er 
         if requests.ShouldStop(e){
             e = requests.ClearAtCurrentFloor(e, nil); // TODO: må gjøre om ClearAtCurrentFloor i  requests.go
             duration += DOOR_OPEN_TIME;
-            e.Dirn = requests_chooseDirection(e);
+            e.Dirn = requests.chooseDirection(e);
             if e.Dirn == D_Stop {
-                return duration; //returnerer dette når den finner ut at det ikke er flere steder å gå
+                return duration; 
             }
         }
         e.Floor += e.Direction;
         duration += TRAVEL_TIME;
     }
 }
-
-func jsonToStateMap(elevID string) (states map[string]elevator.Elevator){
-
-    e := elevstate.StateRestore()
-
-    m = make(map[string]elevator.Elevator)
-
-    m[elevID] = e
-
-    return e
-
-  }
